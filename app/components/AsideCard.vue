fb09d5abd09840e80ae660bc27d54c9680d36bb8
liyang <272891021@qq.com>
2025-07-03 23:03:23 +0800

    refactor(components): destructure props in AsideCard component
    - Destructure props to extract name, icon, and selected variables
    - Update template to use destructured variables
    - Simplify cardClasses computed property

--- app/components/AsideCard.vue
+++ app/components/AsideCard.vue
@@ -1,12 +1,16 @@
 <script lang="ts" setup>
-const props = defineProps({
-  name: { type: String, required: true },
-  icon: { type: String, required: true },
-  selected: { type: Boolean, default: false },
-});
+const {
+  name,
+  icon,
+  selected = false,
+} = defineProps<{
+  name: string;
+  icon: string;
+  selected?: boolean;
+}>();
 
 const cardClasses = computed(() => {
-  return props.selected ? "bg-surface-0 dark:bg-black shadow" : "bg-surface-100 dark:bg-surface-900";
+  return selected ? "bg-surface-0 dark:bg-black shadow" : "bg-surface-100 dark:bg-surface-900";
 });
 </script>
 
@@ -16,8 +20,8 @@ const cardClasses = computed(() => {
     :class="['ripple-box cardClasses rounded-prime-xl w-full border-0 p-3 transition-all duration-100', cardClasses]"
   >
     <div class="flex text-surface-700 dark:text-surface-300">
-      <Icon :name="props.icon" class="mr-2 text-2xl" />
-      <span class="font-bold"> {{ props.name }} </span>
+      <Icon :name="icon" class="mr-2 text-2xl" />
+      <span class="font-bold"> {{ name }} </span>
     </div>
   </div>
 </template>
