043dd95087fe6978c81e63c47880ea979f219bf4
liyang <272891021@qq.com>
2025-07-03 23:25:33 +0800

    feat(keymap): create keymap page and key component
    - Add Key component to display individual keys with k1 and k2 values
    - Create keymap page to show the entire keymap using the Key component
    - Update tailwind.css to include new rounded-prime-md class

---
+++ app/components/Key.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const { k1, k2, select } = defineProps<{
+  k1: string | null;
+  k2: string | null;
+  select?: false | 1 | 2;
+}>();
+</script>
+
+<template>
+  <div class="rounded-prime-md bg-surface-0">
+    <p>{{ k1 ? k1 : "-" }}</p>
+    <p>{{ k2 ? k2 : "-" }}</p>
+  </div>
+</template>
--- app/pages/keymap.vue
+++ app/pages/keymap.vue
@@ -1,7 +1,13 @@
-<template>
-  <div>keymap</div>
-</template>
+<script lang="ts" setup>
+const deviceStore = useDeviceStore();
+</script>
 
-<script lang="ts" setup></script>
+<template>
+  <div class="grid grid-cols-[repeat(14,minmax(0,1fr))] gap-1">
+    <template v-for="key in deviceStore.keymap">
+      <Key :k1="key[0]" :k2="key[1]" />
+    </template>
+  </div>
+</template>
 
 <style></style>
