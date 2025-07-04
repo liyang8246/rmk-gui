580a98cc7c7e3e593dec8111ea7b2a996a93ebed
liyang <272891021@qq.com>
2025-07-05 00:34:10 +0800

    refactor(stores): rename keyboard store to page keymap store
    - Rename `useKeyboardStore` to `usePageKeymapStore`
    - Update store name from "keyboard" to "pageKeymap"

--- app/stores/pageKeymap.ts
+++ app/stores/pageKeymap.ts
@@ -1,4 +1,4 @@
-export const useKeyboardStore = defineStore("keyboard", () => {
+export const usePageKeymapStore = defineStore("pageKeymap", () => {
   const currLayer = ref(0);
   const currKey = ref<[number, number, number]>([0, 0, 0]);
 
