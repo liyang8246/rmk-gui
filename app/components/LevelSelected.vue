53cbfa0f4a027a8bd321ff0e5e7e44c5eca8e983
Menra <112563088+Menra11@users.noreply.github.com>
2025-07-13 16:55:21 +0800

    feat(keymap): implement level selection and update key display logic
    - Add LevelSelected component for level selection
    - Implement level state and changeFloor function in keymap.vue
    - Update labelToDisplay function to use level parameter
    - Adjust layout and styling of keymap display

---
+++ app/components/LevelSelected.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const { selectedNumber = 0 } = defineProps<{
+  selectedNumber?: number;
+}>();
+
+const allLevels = [0, 1, 2, 3];
+
+const emit = defineEmits<{
+  (e: "level", level: number): void;
+}>();
+</script>
+
+<template>
+  <div class="rounded-prime-xl flex gap-1 bg-surface-300/50 p-2 dark:bg-surface-700/50">
+    <ul v-for="index in allLevels" :key="index">
+      <label>
+        <li
+          class="ripple-box cardClasses rounded-prime-xl flex h-10 w-10 cursor-pointer items-center justify-center border-0 text-surface-700 transition-all duration-200 dark:text-surface-300"
+          :class="
+            selectedNumber === index
+              ? 'bg-surface-50 dark:bg-surface-600'
+              : 'bg-surface-200 hover:bg-surface-100 dark:bg-surface-800 dark:hover:bg-surface-700'
+          "
+        >
+          <button @click="emit('level', index)" class="font-medium">{{ index }}</button>
+        </li></label
+      >
+    </ul>
+  </div>
+</template>
+
+<style></style>
--- app/pages/keymap.vue
+++ app/pages/keymap.vue
@@ -1,22 +1,34 @@
 <script lang="ts" setup>
+import LevelSelected from '~/components/LevelSelected.vue';
+
 const keyboardStore = useKeyboardStore();
-function labelToDisplay(label: string): [string | null, string | null] {
+
+const level = ref(0);
+const changeFloor = (f: number) => {
+  level.value = f;
+};
+function labelToDisplay(label: string, level: number): [string | null, string | null] {
   const [row, col] = label.split(",").map(n => parseInt(n, 10));
-  return keyboardStore.indexToDisplay([0, row!, col!]);
+  return keyboardStore.indexToDisplay([level, row!, col!]);
 }
 </script>
 
 <template>
   <div class="flex flex-col">
-    <div class="flex-center my-8">
-      <div class="rounded-prime-md relative h-96 w-11/12 overflow-hidden border border-black bg-surface-300">
-        <template v-for="keys in keyboardStore.kleDefinition?.keys">
-          <Key :keys="labelToDisplay(keys.labels[0]!)" :kleProps="keys" />
-        </template>
+    <div class="m-8 flex flex-col items-center justify-center">
+      <div class="flex w-full items-center justify-start">
+        <LevelSelected @level="changeFloor" :selected-number="level" />
+      </div>
+      <div class="rounded-prime-md relative h-96 w-full overflow-hidden">
+        <div>
+          <template v-for="keys in keyboardStore.kleDefinition?.keys">
+            <Key :keys="labelToDisplay(keys.labels[0]!, level)" :kleProps="keys" />
+          </template>
+        </div>
       </div>
     </div>
     <div class="mx-8">
-      <div class="flex-start flex flex-wrap gap-1">
+      <div class="flex flex-wrap items-start justify-start gap-1">
         <template v-for="[coords, keycode] in keyboardStore.keymap">
           <KeyBoard :keys="keyToDisplay(keycode)" />
         </template>
