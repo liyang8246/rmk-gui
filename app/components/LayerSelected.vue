63bdc4f7c0515497ed29e660c762347cbf78ce4a
Menra <112563088+Menra11@users.noreply.github.com>
2025-07-14 23:35:42 +0800

    refactor(components): improve Key and LayerSelected components' styling and functionality

--- app/components/LayerSelected.vue
+++ app/components/LayerSelected.vue
@@ -12,11 +12,11 @@ const layerIndices = computed(() => {
     <ul v-for="index in layerIndices" :key="index">
       <label>
         <li
-          class="ripple-box cardClasses rounded-prime-xl flex h-10 w-10 cursor-pointer items-center justify-center border-0 text-surface-700 transition-all duration-200 dark:text-surface-300"
+          class="ripple-box cardClasses rounded-prime-xl flex h-8 w-8 cursor-pointer items-center justify-center border-0 text-surface-700 transition-all duration-200 dark:text-surface-300"
           :class="
             pageKeymapStore.currLayer === index
-              ? 'bg-surface-50 dark:bg-surface-600'
-              : 'bg-surface-200 hover:bg-surface-100 dark:bg-surface-800 dark:hover:bg-surface-700'
+              ? 'bg-surface-50 shadow shadow-surface-400 dark:bg-surface-600 dark:shadow-inner dark:shadow-surface-500'
+              : 'bg-surface-200 shadow-inner shadow-surface-400 hover:bg-surface-100 dark:bg-surface-800 dark:shadow-sm dark:shadow-surface-600 dark:hover:bg-surface-700'
           "
         >
           <button class="font-medium" @click="pageKeymapStore.currLayer = index">{{ index }}</button>
