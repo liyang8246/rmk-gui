6a9c723170eea8d737a3345e5ef5aa65d2c7fb10
Menra <112563088+Menra11@users.noreply.github.com>
2025-07-14 21:44:06 +0800

    feat(Key): implement key selection and highlighting

--- app/stores/pageKeymap.ts
+++ app/stores/pageKeymap.ts
@@ -1,9 +1,11 @@
-export const usePageKeymapStore = defineStore('pageKeymap', () => {
-  const currLayer = ref(0)
-  const currKey = ref<[number, number, number]>([0, 0, 0])
+export const usePageKeymapStore = defineStore("pageKeymap", () => {
+  const currLayer = ref(0);
+  const currKey = ref<[number, number, number]>([0, 0, 0]);
+  const keyZone = ref<"outer" | "inner" | null>(null);
 
   return {
     currLayer,
     currKey,
-  }
-})
+    keyZone,
+  };
+});
