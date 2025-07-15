ba315e189dc082bc80aee093d8f66be5f6ad6ee4
Menra <112563088+Menra11@users.noreply.github.com>
2025-07-15 16:02:33 +0800

    feat(keymap): improve key selection and reset functionality and stop key`s emit to effect keymap

--- app/stores/pageKeymap.ts
+++ app/stores/pageKeymap.ts
@@ -2,10 +2,15 @@ export const usePageKeymapStore = defineStore("pageKeymap", () => {
   const currLayer = ref(0);
   const currKey = ref<[number, number, number]>([0, 0, 0]);
   const keyZone = ref<"outer" | "inner" | null>(null);
-
+  function clearSelectedProps() {
+    currKey.value = [0, 0, 0];
+    keyZone.value = null;
+    console.log("clear");
+  }
   return {
     currLayer,
     currKey,
     keyZone,
+    clearSelectedProps,
   };
 });
