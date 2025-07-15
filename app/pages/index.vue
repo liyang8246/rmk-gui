20c0b711c29d3764c2203234379e0e9d6635889d
Menra <112563088+Menra11@users.noreply.github.com>
2025-07-15 16:01:32 +0800

    feat(app): add data selector and improve data display at home

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,26 +1,37 @@
 <script lang="ts" setup>
-const keyboardStore = useKeyboardStore()
+const keyboardStore = useKeyboardStore();
+
+const value = ref("LayerCount");
 </script>
 
 <template>
   <div>
+    <select v-model="value" name="1" id="1">
+      <option value="LayerCount">LayerCount</option>
+      <option value="MacroCount">MacroCount</option>
+      <option value="VialJson">VialJson</option>
+      <option value="Keymap">Keymap</option>
+      <option value="KleDefinition">KleDefinition</option>
+    </select>
+  </div>
+  <div v-if="value == 'LayerCount'">
     <span> LayerCount: </span>
-    <span> {{ keyboardStore.layerCount }} </span>
+    <pre> {{ keyboardStore.layerCount }} </pre>
   </div>
-  <div>
+  <div v-else-if="value == 'MacroCount'">
     <span> MacroCount: </span>
-    <span> {{ keyboardStore.macroCount }} </span>
+    <pre> {{ keyboardStore.macroCount }} </pre>
   </div>
-  <div>
+  <div v-else-if="value == 'VialJson'">
     <span> VialJson: </span>
-    <span> {{ keyboardStore.vialJson }} </span>
+    <pre> {{ keyboardStore.vialJson }} </pre>
   </div>
-  <div>
+  <div v-else-if="value == 'Keymap'">
     <span> Keymap: </span>
-    <span> {{ keyboardStore.keymap }} </span>
+    <pre> {{ keyboardStore.keymap }} </pre>
   </div>
-  <div>
+  <div v-else-if="value == 'KleDefinition'">
     <span> KleDefinition: </span>
-    <span> {{ keyboardStore.kleDefinition }} </span>
+    <pre> {{ keyboardStore.kleDefinition }} </pre>
   </div>
 </template>
