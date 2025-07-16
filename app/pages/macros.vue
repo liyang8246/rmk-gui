039a3e8210fc2805c6b81a70c70537111b611e15
Menra <112563088+Menra11@users.noreply.github.com>
2025-07-16 13:23:03 +0800

    feat(components): add macro selection feature and fix aside navigation

--- app/pages/macros.vue
+++ app/pages/macros.vue
@@ -1,7 +1,13 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>macros</div>
+  <div class="flex flex-col">
+    <div class="justify-cente flex flex-col items-center">
+      <div class="flex w-full items-center justify-start p-2">
+        <MacrosMacroSelected />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style></style>
