598e90c8cac9633a5a2cbed3cb8fbd5c5854f1d7
Menra <112563088+Menra11@users.noreply.github.com>
2025-07-16 14:25:03 +0800

    refactor(keymap): split KeyMap component into subcomponents

---
+++ app/components/KeyMap/MapperPanel.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <template
+    v-for="(i, index) in Object.keys(KeyCode).filter(
+      (key) => !isNaN(Number(key)),
+    )" :key="index"
+  >
+    <KeyMapMappingKey :key-value="keyToConfig(Number(i))" />
+  </template>
+</template>
+
+<style>
+
+</style>
