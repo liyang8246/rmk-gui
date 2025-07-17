fae6833bb2af420c74f4b57805775eb04d9382be
Menra <112563088+Menra11@users.noreply.github.com>
2025-07-18 01:04:47 +0800

    feat(keyboard): add macro functionality and improve UI

--- app/components/Macros/Select.vue
+++ app/components/Macros/Select.vue
@@ -6,14 +6,14 @@ const pageMacrosStore = usePageMacrosStore()
 </script>
 
 <template>
-    <Select
-      v-model="pageMacrosStore.operation[index]"
-      placeholder="select"
-      :options="pageMacrosStore.operationData"
-      option-label="name"
-      class="w-32"
-      :index="index"
-    />
+  <Select
+    v-model="pageMacrosStore.operation[index]"
+    placeholder="select"
+    :options="pageMacrosStore.operationData"
+    option-label="name"
+    class="w-32"
+    :index="index"
+  />
 </template>
 
 <style scoped>
