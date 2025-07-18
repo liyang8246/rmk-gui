c6317feb10c4bf006f63f9b275f360a88400c617
Menra <112563088+Menra11@users.noreply.github.com>
2025-07-19 00:10:42 +0800

    fix(macros): adjust macro deserialization and UI logic

--- app/stores/pageMacros.ts
+++ app/stores/pageMacros.ts
@@ -1,6 +1,5 @@
 export const usePageMacrosStore = defineStore('PageMacros', () => {
   const currMacro = ref(0)
-  const operation = ref(['', '', ''])
-  const operationData = ref<MacroAction[]>(Object.keys(MacroCode).filter(key => !Number.isNaN(Number(key))).map(key => fromMacroCode(fromU8(Number(key)))))
-  return { currMacro, operation, operationData }
+  const operationData = ref<MacroAction[]>(Object.keys(MacroCode).filter(key => !Number.isNaN(Number(key))).filter(key => key !== '1').map(key => fromMacroCode(fromU8(Number(key)))))
+  return { currMacro, operationData }
 })
