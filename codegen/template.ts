57ee15e5cd97a8f6e58c9f3417c459b2b04b799a
liyang <272891021@qq.com>
2025-07-20 00:06:40 +0800

    feat(keycode): enhance keyToInfo function to support composite keycodes
    - Add support for composite keycodes in the keyToInfo function
    - Implement handling of keycodes with multiple parts (e.g., kc followed by another keycode)
    - Improve logic to retrieve KeyInfo for composite keycodes

--- codegen/template.ts
+++ codegen/template.ts
@@ -9,5 +9,15 @@ const keyCodeMap: Record<number, KeyInfo> = {
 }
 
 export function keyToInfo(key: number): KeyInfo | undefined {
-  return keyCodeMap[key]
+  let info = keyCodeMap[key]
+  if (info) return info
+  info = keyCodeMap[key & 0xFF00]
+  if (!info) return
+  if (!info.symbol[0]) return info
+  if (info.symbol[0].includes('kc')) {
+    const k1 = keyCodeMap[key & 0x00FF]
+    if (!k1) return info
+    info.symbol[1] = k1.symbol[1]
+  }
+  return info
 }
