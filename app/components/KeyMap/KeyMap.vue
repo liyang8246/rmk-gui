01252c1a29b36d026994c26a7e517574ce423d19
Menra <112563088+Menra11@users.noreply.github.com>
2025-07-21 16:33:45 +0800

    feat(KeyMap): implement dynamic keyboard resizing

--- app/components/KeyMap/KeyMap.vue
+++ app/components/KeyMap/KeyMap.vue
@@ -1,12 +1,19 @@
 <script lang="ts" setup>
-
+const pageKeymapStore = usePageKeymapStore()
 </script>
 
 <template>
   <div class="flex w-full items-center justify-start pb-4">
     <KeyMapLayerSwitcher />
   </div>
-  <div class="rounded-prime-md relative h-96 w-full overflow-hidden">
+  <div
+    class="rounded-prime-md relative h-96 w-full overflow-hidden"
+    :style="{
+      maxWidth: `${(pageKeymapStore.maxx - 1 + pageKeymapStore.extpending / 2) * 56}px`,
+      maxHeight: `${(pageKeymapStore.maxy - 1 + pageKeymapStore.extpending / 2) * 56}px`,
+
+    }"
+  >
     <KeyMapKeyboardCanvas />
   </div>
 </template>
