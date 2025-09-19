fb0a30784a4227b441358ed5bc2e9fdcfb92a3a2
liyang <272891021@qq.com>
2025-09-19 10:56:44 +0800

    feat(Keyboard): implement key rotation and adjust key padding
    - Add key rotation functionality to the Keyboard component
    - Introduce key padding calculation to replace previous margin ratio
    - Update key styling to include rotation and adjust positioning

--- app/components/common/Keyboard.vue
+++ app/components/common/Keyboard.vue
@@ -4,18 +4,20 @@ const { keys, keySize = 50 } = defineProps<{
   highlight?: StringSet<[number, number]>
   keySize?: number
 }>()
-const keyMargin = computed(() => keySize * 0.15) // Magic Keyboard`s margin ratio
+const keyPadding = computed(() => keySize * 0.13) // Magic Keyboard`s margin ratio
 </script>
 
 <template>
   <div class="relative">
     <key
-      v-for="key in keys" :key="`${key.position}`" :keys="key.info.symbol" @click="(e) => console.log(e)"
-      :style="{
+      v-for="key in keys" :key="`${key.position}`" :keys="key.info.symbol" :key-margin="0" :default-key-size="keySize - keyPadding" :style="{
         position: 'absolute',
         top: `${key.geometry.y * keySize}px`,
         left: `${key.geometry.x * keySize}px`,
+        transform: `rotate(${key.geometry.rotation_angle}deg)`,
+        transformOrigin: `${(key.geometry.rotation_x - key.geometry.x) * keySize}px ${(key.geometry.rotation_y - key.geometry.y) * keySize}px}`,
       }"
+      @click="(e) => console.log(e)"
     />
   </div>
 </template>
