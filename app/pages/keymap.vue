97ec77ad9b5cd8572c5f1f3ffbc293fa81fb126a
liyang <272891021@qq.com>
2025-10-12 22:03:44 -0400

    feat(keymap): refactor layer selection and add key navigation
    - Change `currLayer` from string to number ref with a computed string wrapper
    - Update `keys` computation to use numeric layer directly
    - Add `selectNext` function to navigate to the next available key
    - Automatically select next key after setting a key's value
    - Bind `SelectButton` to the string representation of the current layer

--- app/pages/keymap.vue
+++ app/pages/keymap.vue
@@ -1,9 +1,15 @@
 <script lang="ts" setup>
 const keyboardStore = useKeyboardStore()
 
-const currLayer = ref('0')
+const currLayer = ref(0)
+const currLayerStr = computed({
+  get: () => currLayer.value.toString(),
+  set: (value) => {
+    currLayer.value = Number.parseInt(value)
+  },
+})
 const currKey = ref<[[number, number], 'outer' | 'inner'] | null>(null)
-const keys = computed(() => keyboardStore.fetchKeyList(Number.parseInt(currLayer.value)))
+const keys = computed(() => keyboardStore.fetchKeyList(currLayer.value))
 const layerOption = Array.from({ length: keyboardStore.layerCount! }, (_, i) => i.toString())
 
 const highlight = computed(() => {
@@ -18,11 +24,29 @@ function handleSelected(key: Key, zone: 'outer' | 'inner') {
   currKey.value = isCurr ? null : [[row, col], zone]
 }
 
+function selectNext() {
+  for (let col = currKey.value![0][1] + 1; col < keyboardStore.vialJson!.matrix.cols!; col++) {
+    if (keyboardStore.layoutKeymap!.has([currLayer.value, currKey.value![0][0], col])) {
+      currKey.value = [[currKey.value![0][0], col], 'outer']
+      return
+    }
+  }
+  for (let r = currKey.value![0][0] + 1; r < keyboardStore.vialJson!.matrix.rows! + currKey.value![0][0]; r++) {
+    const row = r % keyboardStore.vialJson!.matrix.rows!
+    for (let col = 0; col < keyboardStore.vialJson!.matrix.cols!; col++) {
+      if (keyboardStore.layoutKeymap!.has([currLayer.value, row, col])) {
+        currKey.value = [[row, col], 'outer']
+        return
+      }
+    }
+  }
+}
+
 function handleSetKey(key: Key) {
   if (!currKey.value) {
     return
   }
-  const currKeyPos: [number, number, number] = [Number.parseInt(currLayer.value), ...currKey.value![0]]
+  const currKeyPos: [number, number, number] = [currLayer.value, ...currKey.value![0]]
   let code = key.info.code
 
   if (currKey.value[1] === 'inner') {
@@ -31,6 +55,7 @@ function handleSetKey(key: Key) {
   }
   keyboardStore.setKeycode(currKeyPos, code)
   keyboardStore.layoutKeymap!.set(currKeyPos, code)
+  selectNext()
 }
 </script>
 
@@ -38,7 +63,7 @@ function handleSetKey(key: Key) {
   <div class="size-full p-3">
     <div class="flex h-full flex-col items-center justify-between">
       <div class="flex w-full justify-start">
-        <SelectButton v-model="currLayer" :allow-empty="false" :options="layerOption" size="small" />
+        <SelectButton v-model="currLayerStr" :allow-empty="false" :options="layerOption" size="small" />
       </div>
       <div class="overflow-hidden">
         <Keyboard :keys="keys" :highlight="highlight" @click="handleSelected" />
