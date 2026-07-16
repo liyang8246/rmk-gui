85e70165d3c9d0bff57b941a9ffdfa3d12d7c367
liyang <272891021@qq.com>
2026-07-16 10:09:06 +0800

    fix(qemu): build with release profile

--- qemu/run.mjs
+++ qemu/run.mjs
@@ -1,14 +1,15 @@
-#!/usr/bin/env node
-// Build the RISC-V firmware, start QEMU with Rynk UART on tcp:7965.
+/* eslint-disable antfu/no-top-level-await */
+/* eslint-disable node/prefer-global/process */
+
 import { spawn } from "node:child_process"
-import { fileURLToPath } from "node:url"
 import { dirname, join } from "node:path"
+import { fileURLToPath } from "node:url"
 
 const dir = dirname(fileURLToPath(import.meta.url))
-const elf = join(dir, "target/riscv32imac-unknown-none-elf/debug/rmk-qemu-riscv")
+const elf = join(dir, "target/riscv32imac-unknown-none-elf/release/rmk-qemu-riscv")
 
 await new Promise((r, f) =>
-  spawn("cargo", ["build"], { cwd: dir, stdio: "inherit" })
+  spawn("cargo", ["build", "--release"], { cwd: dir, stdio: "inherit" })
     .on("exit", (c) => (c ? f(new Error(`cargo exited ${c}`)) : r()))
     .on("error", f))
 
