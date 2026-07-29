0b857480e14fe1b676601746d1a71b8c8e2a64cc
liyang <272891021@qq.com>
2026-07-29 17:09:00 +0800

    refactor: inline ToolButton into ToolsBar via snippet
    Merge ToolButton.svelte into ToolsBar.svelte using Svelte 5 {#snippet} + {@render} pattern.

--- src/components/ToolsBar.svelte
+++ src/components/ToolsBar.svelte
@@ -1,8 +1,21 @@
 <script lang='ts'>
-  import ToolButton from './ToolButton.svelte'
+  import Icon from '@iconify/svelte'
 
   function noop() {}
 </script>
+
+{#snippet toolButton(name: string, icon: string)}
+  <button
+    class='
+      flex cursor-pointer items-center gap-1 rounded-xl p-2 text-base-content
+      hover:bg-base-300
+    '
+    onclick={noop}
+  >
+    <Icon {icon} />
+    <span class='text-sm'>{name}</span>
+  </button>
+{/snippet}
 
 <div
   class='
@@ -10,10 +23,10 @@
     shadow-lg ring ring-base-300
   '
 >
-  <ToolButton name='Layers' icon='lucide:layers' onClick={noop} />
-  <ToolButton name='Macros' icon='lucide:zap' onClick={noop} />
-  <ToolButton name='Combos' icon='lucide:combine' onClick={noop} />
-  <ToolButton name='Wireless' icon='lucide:bluetooth' onClick={noop} />
-  <ToolButton name='Firmware' icon='lucide:cpu' onClick={noop} />
-  <ToolButton name='Setting' icon='lucide:settings' onClick={noop} />
+  {@render toolButton('Layers', 'lucide:layers')}
+  {@render toolButton('Macros', 'lucide:zap')}
+  {@render toolButton('Combos', 'lucide:combine')}
+  {@render toolButton('Wireless', 'lucide:bluetooth')}
+  {@render toolButton('Firmware', 'lucide:cpu')}
+  {@render toolButton('Setting', 'lucide:settings')}
 </div>
